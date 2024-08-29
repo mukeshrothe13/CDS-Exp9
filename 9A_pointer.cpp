951b8a058e6db7abf9ffa58d47f01814e3676101
mukeshrothe13 <rothemukesh7@gmail.com>
2024-08-29 15:50:03 +0530

    9A_pointer.cpp

---
+++ 9A_pointer.cpp
@@ -0,0 +1,17 @@
+//Mukesh Rothe  //23070123089  //EXP9
+#include <iostream>
+using namespace std;
+
+int main() {
+    char b = 's';
+    char*ptr = &b;
+   
+    cout<<"The value pointed by *ptr is  "<<b;
+    cout<<"\nThe value in b is " <<b;
+    cout<<"\nValue of pointer variable *ptr is  "<<(void*) ptr;
+    cout<<"\nAddress variable of b is   "<<(void*)&b;
+    ptr++;
+    cout<<"\nIncrement value of ptr is "<<(void*)ptr;
+
+    return 0;
+}
