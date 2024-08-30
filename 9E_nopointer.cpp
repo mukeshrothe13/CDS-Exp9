5351b6e648c115b50a257c336bfb756b488dfc52
mukeshrothe13 <rothemukesh7@gmail.com>
2024-08-30 22:49:45 +0530

    9E_nopointer.cpp

---
+++ 9E_nopointer.cpp
@@ -0,0 +1,16 @@
+//Mukesh Rothe 23070123089 EXP9
+#include<iostream>
+using namespace std;
+int main()
+{
+    int *ptr;
+    int a[5] = { 1,2,4,8,9};
+    ptr = &a[0];
+    int i;
+    for(i=0 ; i<5 ; i++)
+    {
+        cout << "Element "<< i+1 <<" "<<"="<<" "<<*(a+i) << endl;
+        ptr ++;
+
+    }
+}
